28fcd6fa684c9f0427b0b68296ef4a16ed51d40c
Jammel Johnson <jammel.johnson@myguardiangroup.com>
2024-08-25 11:50:16 -0500

    setup basic cake container

---
+++ src/components/CakeContainer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function CakeContainer() {
+  return (
+    <di>
+      <h2>Number Of cakes</h2>
+      <button>Buy Cake</button>
+    </di>
+  );
+}
+
+export default CakeContainer;
