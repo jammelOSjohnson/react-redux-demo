a1227ab12e237897617f2c43a87522b83d4ac595
Jammel Johnson <jammel.johnson@myguardiangroup.com>
2024-08-25 14:41:20 -0500

    connect the Icecream componet to the store using react-redux hooks

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import { Provider } from "react-redux";
 import store from "./redux/store";
 import HooksCakeContainer from "./components/HooksCakeContainer";
 import IceCreamContainer from "./components/IceCreamContainer";
+import HooksIceCreamContainer from "./components/HooksIceCreamContainer";
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
+        <HooksIceCreamContainer />
         <IceCreamContainer />
         <HooksCakeContainer />
         <CakeContainer />
