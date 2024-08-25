8ccbf81e298249d29fdc1f084f649dd3bd8db44c
Jammel Johnson <jammel.johnson@myguardiangroup.com>
2024-08-25 16:26:16 -0500

    exaple of using the ownProps parameter

--- src/App.js
+++ src/App.js
@@ -8,11 +8,14 @@ import IceCreamContainer from "./components/IceCreamContainer";
 import HooksIceCreamContainer from "./components/HooksIceCreamContainer";
 import CarComponent from "./components/CarComponent";
 import NewCakeContainer from "./components/NewCakeContainer";
+import ItemContainer from "./components/ItemContainer";
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
+        <ItemContainer cake />
+        <ItemContainer />
         <CarComponent />
         <HooksIceCreamContainer />
         <IceCreamContainer />
---
+++ src/components/ItemContainer.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { connect } from "react-redux";
+import { buyCake, buyIceCream } from "../redux";
+
+function ItemContainer(props) {
+  return (
+    <div>
+      <h2>Item - {props.item} </h2>
+      <button onClick={() => props.buyItem()}>Buy Item</button>
+    </div>
+  );
+}
+
+const mapStateToProps = (state, ownProps) => {
+  const itemState = ownProps.cake
+    ? state.cake.numOfCakes
+    : state.iceCream.numOfIceCreams;
+
+  return {
+    item: itemState,
+  };
+};
+
+const mapDispatchToProps = (dispatch, ownProps) => {
+  const dispatchFunction = ownProps.cake
+    ? () => dispatch(buyCake())
+    : () => dispatch(buyIceCream());
+
+  return {
+    buyItem: dispatchFunction,
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ItemContainer);
+// If you dont want to subscribe to changes pass null as the first value
+//export default connect(null, mapDispatchToProps)(ItemContainer);
