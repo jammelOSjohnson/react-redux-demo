425ed899b102df026477fcb9226b53beb2931196
Jammel Johnson <jammel.johnson@myguardiangroup.com>
2024-08-25 17:11:17 -0500

    added a user slice to the store

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -2,11 +2,13 @@ import { combineReducers } from "redux";
 import cakeReducer from "./cake/cakeReducer";
 import iceCreamReducer from "./iceCream/iceCreamReducer";
 import carReducer from "./car/carReducer";
+import userReducer from "./user/userReducer";
 
 const reducer = combineReducers({
   cake: cakeReducer,
   iceCream: iceCreamReducer,
   car: carReducer,
+  user: userReducer,
 });
 
 export default reducer;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,12 @@
 import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
 import reducer from "./rootReducer";
 import logger from "redux-logger";
 import { composeWithDevTools } from "redux-devtools-extension";
 
 const store = createStore(
   reducer,
-  composeWithDevTools(applyMiddleware(logger))
+  composeWithDevTools(applyMiddleware(logger, thunk))
 );
 
 export default store;
