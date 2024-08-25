0ea0436c23cb53dac3330184b0f7a1f9c463c2ab
Jammel Johnson <jammel.johnson@myguardiangroup.com>
2024-08-25 13:01:09 -0500

    comment out logo

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from "./logo.svg";
+//import logo from "./logo.svg";
 import "./App.css";
 import CakeContainer from "./components/CakeContainer";
 import { Provider } from "react-redux";
